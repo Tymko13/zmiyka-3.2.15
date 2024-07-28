8b2ea9506b2399ca42555ef98bbc77b90a912741
Tymko <kondrashko13@proton.me>
2024-07-28 23:06:55 +0300

    Імпортував pygame

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print("Hello world!")
-for n in range(10):
-    print("Hello, " + str(n + 1) + "!")
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+    pygame.display.flip()
+
+    clock.tick(60)
+pygame.quit()
