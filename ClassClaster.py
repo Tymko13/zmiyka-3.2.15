d126e6859305347b4f0a5b285c9d13e10790e94d
muvi-K <shaharovdanil@gmail.com>
2024-07-30 00:08:56 +0200

    movement, dying and func-improvements

--- ClassClaster.py
+++ ClassClaster.py
@@ -1,3 +1,4 @@
+from email.mime import application
 import pygame as py
 from enum import Enum
 from collections import deque
@@ -8,6 +9,7 @@ FIELD_SIZE = 25  # Ширина та висота поля в клітинках
 COOL_BLUE_COLOR = (100, 100, 200)
 COOL_RED_COLOR = (200, 100, 100)
 COOL_YELLOW_COLOR = (200, 200, 100)
+COOL_GREEN_COLOR = (50, 150, 50)
 
 
 class Position:
@@ -71,17 +73,70 @@ class Position:
 
 class State(Enum):
     EMPTY = 0
+    APPLE = 3
     TAIL = 10
     HEAD_LEFT = Position(-1, 0)
     HEAD_UP = Position(0, -1)
     HEAD_RIGHT = Position(1, 0)
     HEAD_DOWN = Position(0, 1)
+    
 
+class Field:
+    field = []
+    snakes = []
+
+    class Square:
+        state = State.EMPTY
+
+        def __init__(self, x: int, y: int):
+            self.rect = (x, y, SQUARE_SIZE, SQUARE_SIZE)
+
+        def draw(self, window: py.Surface):
+            if self.state == State.EMPTY:
+                color = COOL_BLUE_COLOR
+            elif self.state == State.APPLE:
+                color = COOL_RED_COLOR
+            elif self.state == State.TAIL:
+                color = COOL_YELLOW_COLOR
+            else:
+                color = COOL_GREEN_COLOR
+            py.draw.rect(window, color, self.rect, 10)
+
+    def __init__(self):
+        for i in range(FIELD_SIZE):
+            self.field.append([])  # Створює рядки поля
+            for j in range(FIELD_SIZE):
+                self.field[-1].append(Field.Square(j * SQUARE_SIZE, i * SQUARE_SIZE))  # Створює клітинки рядків
+
+    def spawn_snake(self, head_position: Position, facing: State, length: int) -> None:
+        self.snakes.append(Snake(head_position, facing, length, self))
+
+    def spawn_apple(self, position: Position) -> bool:
+        if self.get_square_state(position) == State.EMPTY:
+            self.set_square_state(position, State.APPLE)
+            return True
+        return False
+
+    def set_square_state(self, position: Position, state: State) -> None:
+        self.field[position.y][position.x].state = state
+
+    def get_square_state(self, position: Position) -> State:
+        return self.field[position.y][position.x].state
+
+    def move_snake(self, index: int, direction: State) -> None:
+        self.snakes[index].move(direction)
+
+    def draw(self, window: py.Surface) -> None:
+        for row in self.field:
+            for square in row:
+                square.draw(window)
+                
 
 class Snake:
     snake = deque()
+    food = 0
 
-    def __init__(self, head_position, facing, length, field):
+    def __init__(self, head_position: Position, facing: State, length: int, field: Field):
         self.direction = facing
         self.field = field
         self.snake.append(head_position.copy())
@@ -91,42 +146,40 @@ class Snake:
             self.snake.append(head_position.copy())
             self.field.set_square_state(head_position, State.TAIL)
 
-    def move(self):
-        self.field.set_square_state(self.snake[0], State.TAIL)
-        self.snake.appendleft(self.snake[0] + self.direction.value)
-        self.field.set_square_state(self.snake[0], self.direction)
-        self.field.set_square_state(self.snake.pop(), State.EMPTY)
+    def __del__(self):
+        for part in self.snake:
+            self.field.set_square_state(part, State.EMPTY)
 
-class Field:
-    field = []
-    snakes = []
+    def move(self, direction: State) -> None:
+        if self.direction.value != -direction.value:
+            self.direction = direction
+        # else don't change
 
-    class Square:
-        def __init__(self, x, y, state=State.EMPTY):
-            self.rect = (x, y, SQUARE_SIZE, SQUARE_SIZE)
-            self.state = state
+        old_head_position = self.snake[0].copy()  # temp
+        
+        self.field.set_square_state(old_head_position, State.TAIL)  # making old head a tail
+        if self.food != 0:
+            self.food -= 1
+        else:
+            self.field.set_square_state(self.snake.pop(), State.EMPTY)  # delete tail 
+        
+        new_head_position = old_head_position + self.direction.value
+        
+        if new_head_position.x < 0 or new_head_position.x >= FIELD_SIZE:
+            self.field.snakes.remove(self)
+            return
+        if new_head_position.y < 0 or new_head_position.y >= FIELD_SIZE:
+            self.field.snakes.remove(self)
+            return
 
-        def draw(self, window):
-            if self.state == State.EMPTY:
-                color = COOL_BLUE_COLOR  # Блакитний
-            else:
-                color = COOL_YELLOW_COLOR  # Червоний
-            py.draw.rect(window, color, self.rect, 10)
-
-    def __init__(self):
-        for i in range(FIELD_SIZE):
-            self.field.append([])  # Створює рядки поля
-            for j in range(FIELD_SIZE):
-                self.field[-1].append(Field.Square(j * SQUARE_SIZE, i * SQUARE_SIZE))  # Створює клітинки рядків
-
-    def spawn_snake(self, head_position, facing, length):
-        if len(self.snakes) == 0:
-            self.snakes.append(Snake(head_position, facing, length, self))
-
-    def set_square_state(self, position, state):
-        self.field[position.y][position.x].state = state
-
-    def draw(self, window):
-        for row in self.field:
-            for square in row:
-                square.draw(window)
+        old_state = self.field.get_square_state(new_head_position)
+        
+        if old_state == State.APPLE:
+            self.food += State.APPLE.value
+        elif old_state != State.EMPTY:
+            self.field.snakes.remove(self)
+            return
+        
+        self.snake.appendleft(new_head_position)  # adding a new head
+        self.field.set_square_state(self.snake[0], self.direction)  # drawing new! head
+        
--- main.py
+++ main.py
@@ -7,14 +7,17 @@ fieldSize = (750, 750)
 window = py.display.set_mode(fieldSize)
 
 field = cl.Field()
-field.spawn_snake(cl.Position(5, 7), cl.State.HEAD_UP, 6)
-
+field.spawn_snake(cl.Position(0, 0), cl.State.HEAD_DOWN, 1)
+field.spawn_apple(cl.Position(0, 5))
+field.spawn_apple(cl.Position(0, 7))
+field.spawn_apple(cl.Position(0, 12))
+field.spawn_apple(cl.Position(0, 13))
+field.spawn_apple(cl.Position(0, 20))
 
 def draw_game_window():
     window.fill((0, 0, 0))
     field.draw(window)
     py.display.update()
-
 
 running = True
 while running:
@@ -22,10 +25,11 @@ while running:
         if event.type == py.QUIT:
             running = False
 
+    #TODO : Game logic
+
     draw_game_window()
-    py.time.delay(1000)
-    field.snakes[0].move()
-    print("ВСЕ ПРАЦЮЄ")
+    py.time.delay(2000)
+    field.move_snake(0, cl.State.HEAD_DOWN)
 
     clock.tick(60)  # FPS
 
